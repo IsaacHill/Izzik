d642a8418987945e50dd4f015fed89405d537819
Xagefu <xagefu@gmail.com>
2015-05-30 20:59:53 +1000

    fixed running and dance modes colour change etc

--- light-belt/light-belt.ino
+++ light-belt/light-belt.ino
@@ -28,15 +28,24 @@ float currentDirection = 0;
 float previousDirection = 0;
 //Variable for dance mode that defines light positions initilized to
 //3 i.e the fourth LED.
-int danceLocationC = 0;
-int danceLocationN = 0;
+int danceLocation = 3;
+//Variable to define direction of LED motion in dancing mode.
+//Initialize to 1 (positive).
+int danceDirection = 1;
+
+
 //Initialize mode as unset, 1 defines dancing 2 defines running.
 int mode = 0;
-
+int changeAccelColour = 4;
+int colourLED = 0;
 //Constants for running mode that define breaking.
 #define BRAKETHRESHOLD    200
 #define BRAKETIME         200
-
+//Constants for accelerometer normalization.
+float xMin = -2040;
+float xMax = 2040;
+float yMin = -2040;
+float yMax = 2040;
 //defines whether the user is currently slowing down
 //0 defines not currently slowing down, 1 defines that they are i.e they
 //are slowing down at faster than the BRAKETHRESHOLD. If braking is equal to
@@ -44,9 +53,19 @@ int mode = 0;
 //longer. Initialize to 0, not breaking.
 int braking = 0;
 
+//left counter variable for running
+int leftIn = 2;
+//right counter variable for running
+int rightIn = 3;
 
 int set = 0;
 float i = 0;
+
+//the timer used for detecting when breaks must be cleared
+long breakTime;
+
+unsigned long interval = 500;
+
 
 //Initialize strip (chain of leds), first input is number of leds in chain.
 // Parameter 1 = number of pixels in strip
@@ -57,27 +76,58 @@ float i = 0;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
 
 //Array of colours arranged in order of severity/intensity
-uint32_t colorArray [10] = {strip.Color(255, 255, 255),
-                            strip.Color(153, 204, 255),
+uint32_t colorArray [40] = {strip.Color(32, 32, 32),
+                            strip.Color(0, 0, 102),
+                            strip.Color(0, 0, 153),
+                            strip.Color(76, 0, 153),
+                            strip.Color(0, 102, 104),
                             strip.Color(0, 0, 255),
+                            strip.Color(127, 0, 255),
+                            strip.Color(153, 51, 255),
+                            strip.Color(153, 0, 153),
+                            strip.Color(204, 0, 204),
+                            strip.Color(153, 0, 76),
+                            strip.Color(255, 0, 255),
+                            strip.Color(255, 0, 127),
+                            strip.Color(255, 51, 255),
+                            strip.Color(255, 51, 153),
+                            strip.Color(51, 102, 0),
+                            strip.Color(0, 102, 0),
+                            strip.Color(76, 153, 0),
+                            strip.Color(0, 153, 0),
+                            strip.Color(0, 204, 0),
+                            strip.Color(128, 255, 0),
+                            strip.Color(0, 255, 0),
                             strip.Color(153, 255, 51),
-                            strip.Color(0, 204, 204),
-                            strip.Color(204, 204, 204),
-                            strip.Color(251, 51, 153),
+                            strip.Color(51, 255, 51),
                             strip.Color(255, 255, 0),
                             strip.Color(255, 128, 0),
-                            strip.Color(255, 0, 0)
+                            strip.Color(255, 255, 51),
+                            strip.Color(255, 153, 51),
+                            strip.Color(255, 255, 102),
+                            strip.Color(255, 178, 102),
+                            strip.Color(255, 204, 204),
+                            strip.Color(255, 0, 0),
+                            strip.Color(255, 51, 51),
+                            strip.Color(255, 102, 102),
+                            strip.Color(255, 153, 153),
+                            strip.Color(255, 204, 204),
+                            strip.Color(255, 229, 204),
+                            strip.Color(255, 255, 204),
+                            strip.Color(229, 255, 204),
+                            strip.Color(96, 96, 96),
                            };
-
+//Due to the nature of assignments and pointers in c
+//this variable contains the soze of the colorArray, for
+//ease of use.
+int colourSize = 40;
 
 
 void setup() {
   strip.begin();
-  strip.setBrightness(20);
-  while (!Serial);
+  strip.setBrightness(255);
   strip.show();
   Serial.begin(9600);
-  Serial.println("currentelerometer Test"); Serial.println("");
   // initialize the digital pin as an output.
   if (!lsm.begin())
   {
@@ -122,10 +172,8 @@ void loop() {
   }
   while (mode  == 1) {
     dancingMode();
-    Serial.println("Dancing");
   }
   while (mode == 2) {
-    Serial.println("Running");
     runningMode();
     //Placeholder light cycling for testing, (lights stop cycling
     //if a mode is chosen or the code hangs for whatever reason).
@@ -138,9 +186,21 @@ void loop() {
 void getValues() {
   prevX = currentX;
   prevY = currentY;
+  previousDirection = currentDirection;
   lsm.read();
   currentX = (int)lsm.accelData.x;
   currentY = (int)lsm.accelData.y;
+  currentDirection = (atan2(lsm.magData.y, lsm.magData.x) * 180) / Pi;
+  if (changeAccelColour % 4 == 0) {
+    //Normalize acceleration
+    float xNorm = abs(currentX) / xMax;
+    float yNorm = abs(currentY) / yMax;
+    float crossXY = sqrt(xNorm * yNorm);
+    colourLED = colourSize * crossXY;
+    Serial.print("Colour Choice ");
+    Serial.println(colourLED);
+  }
+  changeAccelColour += 1;
 }
 
 //Detects if the users motion is setting a mode and if so
@@ -153,12 +213,12 @@ void detectMode() {
   float acceleration = abs(prevX - currentX);
   Serial.println("rotation");
   Serial.println(rotation);
-  if (rotation > 30 ) {
+  if (rotation > 15 ) {
     mode = 1;
   }
   //Detects if user is accelerating forward and isnt rotating
   //and if so sets belt to running mode.
-  else if (acceleration > 400) {
+  else if (acceleration > 100) {
     mode = 2;
   }
 }
@@ -169,129 +229,84 @@ void dancingMode() {
   setDance();
   //change denotes the change in degrees from the previous
   //read of the heading.
-  int change = currentDirection - previousDirection;
-  if (abs(currentDirection - previousDirection) > 25) {
-    danceLocationN = danceLocationC;
-    if (change < 0) {
-      if (change / 25 > 4) {
-        danceLocationN -= 4;
-      }
-      else {
-        danceLocationN -= change / 25;
-      }
+  int changeDegrees = currentDirection - previousDirection;
+  if (abs(changeDegrees) > 15) {
+    Serial.print("Im Changing Modes to:  ");
+    Serial.println(changeDegrees);
+    if (changeDegrees > 0) {
+      Serial.println("positive");
+      danceDirection = 1;
     }
     else {
-      if (change / 25 > 4) {
-        danceLocationN += 4;
-      }
-      else {
-        danceLocationN += change / 25;
-      }
+      Serial.println("Negative");
+      danceDirection = 0;
     }
   }
   delay(100);
 }
 
+
 void setDance() {
-  //Change should never be more than 4 unless the current LED
-  //position is traveling from the LED at 0 to the LED at 15.
-  int change = danceLocationN - danceLocationC;
-  //Detects if we are going over the front of the belt
-  //i.e traveling from led at position 0 to led at position 15.
-  if (abs(change) > 4 && danceLocationC == 0) {
-    setDanceLights(0);
+  if (danceDirection == 1) {
+    //Set new pixels to colour
+    strip.setPixelColor((danceLocation + 1) % 16, colorArray[colourLED]);
+    strip.setPixelColor((danceLocation + 5) % 16, colorArray[colourLED]);
+    strip.setPixelColor((danceLocation + 9) % 16, colorArray[colourLED]);
+    strip.setPixelColor((danceLocation + 13) % 16, colorArray[colourLED]);
+    //Reset Previous pixels to non rotating colour.
+    strip.setPixelColor(danceLocation, colorArray[6]);
+    strip.setPixelColor((danceLocation + 4) % 16, colorArray[colourLED - 1]);
+    strip.setPixelColor((danceLocation + 8) % 16, colorArray[colourLED - 1]);
+    strip.setPixelColor((danceLocation + 12) % 16, colorArray[colourLED - 1]);
+    danceLocation = (danceLocation + 1) % 16;
   }
-  else if (abs(change) > 4 && danceLocationC == 15) {
-    setDanceLights(1);
+  else {
+    //Set new pixels to colour
+    strip.setPixelColor((danceLocation + 15) % 16, colorArray[colourLED]);
+    strip.setPixelColor((danceLocation + 11) % 16, colorArray[colourLED]);
+    strip.setPixelColor((danceLocation + 7) % 16, colorArray[colourLED]);
+    strip.setPixelColor((danceLocation + 3) % 16, colorArray[colourLED]);
+    //Reset Previous pixels to non rotating colour.
+    strip.setPixelColor(danceLocation, colorArray[6]);
+    strip.setPixelColor((danceLocation + 4) % 16, colorArray[colourLED - 1]);
+    strip.setPixelColor((danceLocation + 8) % 16, colorArray[colourLED - 1]);
+    strip.setPixelColor((danceLocation + 12) % 16, colorArray[colourLED - 1]);
+    danceLocation = (danceLocation + 15) % 16;
   }
-  //Change in LED direction will be in the negative direction.
-  else if (change < 0) {
-    setDanceLights(2);
-  }
-  //Change in LED direction will be in the positive direction.  
-  else if (change > 0) {
-    setDanceLights(3);
-  }
+  strip.show();
 }
 
-//Sets the lights based on the danceLocationC and 
+//Sets the lights based on the danceLocationC and
 //the danceLocationN. direction defines how what direction around the belt
-//the lights will be travelling, 0 and 1 define crossing between LED 0 and 
+//the lights will be travelling, 0 and 1 define crossing between LED 0 and
 //15, while 2 and 3 define traveling in either direction around the belt.
 void setDanceLights(int direction) {
-  if(i == 0){
-    //Set 0 to white.
-    strip.setPixelColor(0, colorArray[0]);
-    //Set 15 to colour
-    strip.setPixelColor(15, colorArray[2]);
-    //Now set other three lights based on this position
-    strip.setPixelColor(3, colorArray[2]);
-    strip.setPixelColor(7, colorArray[2]);
-    strip.setPixelColor(11, colorArray[2]);
-    //Reset the previous positions
-    strip.setPixelColor(4, colorArray[0]);
-    strip.setPixelColor(8, colorArray[0]);
-    strip.setPixelColor(12, colorArray[0]);
-    danceLocationC = 15;
-  }
-  else if(i == 1){
-    //Set 15 to White
-    strip.setPixelColor(15, colorArray[0]);
-    //Set 0 to colour
-    strip.setPixelColor(0, colorArray[2]);
-    strip.setPixelColor(4, colorArray[2]);
-    strip.setPixelColor(8, colorArray[2]);
-    strip.setPixelColor(12, colorArray[2]);
-    danceLocationC = 0;
-  }
-  else if(i == 2){
-    //Set previous to white.
-    strip.setPixelColor(danceLocationC, colorArray[0]);
-    //Set next to colour
-    strip.setPixelColor(danceLocationC + 1, colorArray[2]);
-    //Now set other three lights based on this position.
-    strip.setPixelColor((danceLocationC + 4)%15, colorArray[2]);
-    strip.setPixelColor((danceLocationC + 8)%15, colorArray[2]);
-    strip.setPixelColor((danceLocationC + 12)%15, colorArray[2]);
-    //Reset the previous positions
-    strip.setPixelColor((danceLocationC + 5)%15, colorArray[0]);
-    strip.setPixelColor((danceLocationC + 9)%15, colorArray[0]);
-    strip.setPixelColor((danceLocationC + 13)%15, colorArray[0]);
-    danceLocationC += 1;
-  }
-  else if(1 == 3){
-    //Set previous to white.
-    strip.setPixelColor(danceLocationC, colorArray[0]);
-    //Set next to colour
-    strip.setPixelColor(danceLocationC + 1, colorArray[3]);
-    //Now set other three lights based on this position.
-    strip.setPixelColor((danceLocationC + 4)%15, colorArray[2]);
-    strip.setPixelColor((danceLocationC + 8)%15, colorArray[2]);
-    strip.setPixelColor((danceLocationC + 12)%15, colorArray[2]);
-    //Reset the previous positions
-    strip.setPixelColor((danceLocationC + 5)%15, colorArray[0]);
-    strip.setPixelColor((danceLocationC + 9)%15, colorArray[0]);
-    strip.setPixelColor((danceLocationC + 13)%15, colorArray[0]);
-    danceLocationC -= 1;
-  }
+
 }
 
 //Mode that loops when mode is set to running
 void runningMode() {
-  int k = 7;
-  int j = 8;
-  while (j < 16) {
-    runningSetPixel(k, j);
-    k--;
-    j++;
+    runningSetPixel();
+    
+    if (--leftIn%16 == -1) {
+    	leftIn = 15;
+    }
+    if(leftIn == 10) {
+       leftIn = 2;
+       rightIn = 3;
+    }
+    else{ 
+      rightIn++;
+    }
     delay(200);
-  }
+  
 }
 
-void runningSetPixel(int k, int j) {
+void runningSetPixel() {
   getValues();
   checkBraking();
   if (braking == 1) {
+    breakTime = millis();
     showBraking();
   }
   //If braking  = 2 then the checkBraking just
@@ -301,29 +316,32 @@ void runningSetPixel(int k, int j) {
     //Re initialize braking.
     braking = 0;
   }
-  if (braking == 1 && k == 7 || k == 6) {
-    if ( k == 7) {
-      strip.setPixelColor(0, colorArray[0]);
-      strip.setPixelColor(15, colorArray[0]);
-
-    }
-    else {
-      strip.setPixelColor(k - 1, colorArray[0]);
-      strip.setPixelColor(j - 1, colorArray[0]);
-
+  if (braking == 1 && leftIn == 9 || leftIn == 10) {
+    if (leftIn == 10) {
+       strip.setPixelColor(leftIn - 1, colorArray[0]);
+       strip.setPixelColor(rightIn - 1, colorArray[0]);
     }
   }
   else {
-    strip.setPixelColor(k, colorArray[2]);
-    strip.setPixelColor(j, colorArray[2]);
-    //Reset previous leds to white.
-    if (k != 7 ) {
-      strip.setPixelColor(k + 1, colorArray[0]);
-      strip.setPixelColor(j - 1, colorArray[0]);
-    }
-    else {
-      strip.setPixelColor(0, colorArray[0]);
-      strip.setPixelColor(15, colorArray[0]);
+    Serial.print("this is the k");
+    Serial.println(leftIn);
+    strip.setPixelColor(leftIn, colorArray[2]);
+    strip.setPixelColor(rightIn, colorArray[2]);
+    //Reset previous led to white.
+    if (leftIn == 2) {
+      strip.setPixelColor(10, colorArray[0]);
+      strip.setPixelColor(11, colorArray[0]);
+      strip.setPixelColor(leftIn, colorArray[2]);
+      strip.setPixelColor(rightIn, colorArray[2]);   
+    } else {
+      if (leftIn != 11) {
+      	strip.setPixelColor((leftIn+1)%16, colorArray[0]);
+      	strip.setPixelColor(rightIn-1, colorArray[0]);
+      } else {
+        strip.setPixelColor((leftIn+1)%16, colorArray[0]);
+      	strip.setPixelColor(rightIn-1, colorArray[0]);
+  
+      }
     }
   }
   strip.show();
@@ -348,19 +366,20 @@ void checkBraking() {
 
 //Clear breaking when no breaking is detected.
 void clearBraking() {
-  strip.setPixelColor(0, colorArray[0]);
-  strip.setPixelColor(1, colorArray[0]);
-  strip.setPixelColor(2, colorArray[0]);
-  strip.setPixelColor(3, colorArray[0]);
-
+  if (millis() - breakTime > interval) {
+    strip.setPixelColor(9, colorArray[0]);
+    strip.setPixelColor(10, colorArray[0]);
+    strip.setPixelColor(11, colorArray[0]);
+    strip.setPixelColor(12, colorArray[0]);
+  }
 }
 
 //Set rear lights to red to denote breaking.
 void showBraking() {
-  strip.setPixelColor(0, colorArray[9]);
-  strip.setPixelColor(1, colorArray[9]);
-  strip.setPixelColor(2, colorArray[9]);
-  strip.setPixelColor(3, colorArray[9]);
+  strip.setPixelColor(9, colorArray[31]);
+  strip.setPixelColor(10, colorArray[31]);
+  strip.setPixelColor(11, colorArray[31]);
+  strip.setPixelColor(12, colorArray[31]);
   strip.show();
 }
 
